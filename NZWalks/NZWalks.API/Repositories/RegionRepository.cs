e21fb5c15f3e565cc50f45887c166beecfe7d74d
Thupden <thupdenbhutia281199@gmail.com>
2022-10-12 20:53:23 +0530

    end of section 3

---
+++ NZWalks/NZWalks.API/Repositories/RegionRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public class RegionRepository : IRegionRepository
+    {
+        private NZWalksDbContext nzWalksDbContext;
+
+        public RegionRepository(NZWalksDbContext nzWalksDbContext)
+        {
+            this.nzWalksDbContext = nzWalksDbContext;
+        }
+        public async Task<IEnumerable<Region>> GetAllAsync()
+        {
+            return await nzWalksDbContext.Regions.ToListAsync();
+        }
+    }
+}
